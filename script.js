a63839d7208c70dfe3d60e307cb4eb080a7d92a7
Mithila <mithilarahaman410@gmail.com>
2024-10-16 22:01:38 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const myNumbers = [1, 2, 3, 4, 5];
+
+function sumArray(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  }
+  
+  console.log(sumArray(myNumbers)); 
